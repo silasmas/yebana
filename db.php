18b7b1247de96569d0dad677fd1d6202ca5bac64
Pacretaria <pacrette2020@gmail.com>
2025-09-09 16:12:04 +0200

    Update database connection credentials
    Changed the PDO connection to use new host, database name, username, and password. This update reflects a migration to a different database configuration.

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $connect = NULL;
 $recherche = null;
 try
 {
-	$db = new PDO('mysql:host=localhost;dbname=trans2524376;charset=utf8;', 'root', '');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=u911414181_yebana;charset=utf8;', 'u911414181_yebana', 'Yebana2025@');
     $connect = 'connect';
 }
 catch(Exception $e)
